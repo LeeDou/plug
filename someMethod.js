b9884203649c67acb8c1b75ef8c08aa24ff4abfb
LeeDou <1040241228@qq.com>
2017-02-21 09:05:43 +0800

    增加event方法

--- someMethod.js
+++ someMethod.js
@@ -53,4 +53,25 @@
 		}
 
 	};
+	window.kantMT.EventUtil = {
+		addHander:function(element,type,handler){
+			if (element.addEventListener) {
+				element.addEventListener(type,handler,false);
+			} else if (element.attahEvent) {
+				element.attachEvent("on"+type,handler);
+			} else {
+				element["on"+type] = handler;
+			}
+		}
+		removeHander:function(element.type,handler){
+			if (element.removeEventListener) {
+				element.removeEventListener(type,handler
+					,false)
+			} else if (element.detachEvent) {
+				element.detachEvent("on"+type,handler);
+			} else {
+				element["on"+type] = null;
+			}
+		}
+	};
 })()
